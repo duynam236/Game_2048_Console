b2787ba85472b9dbc06c21bb0089cac0f7ee5726
duynam236 <38345095+duynam236@users.noreply.github.com>
2018-05-11 13:56:01 +0700

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -50,24 +50,6 @@ void gotoXY(int column, int line)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-// Ham xoa man hinh
-void DeleteScreen() {
-	CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;
-	HANDLE	hConsoleOut;
-	COORD	Home = { 0, 0 };
-	DWORD	dummy;
-
-	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleScreenBufferInfo(hConsoleOut, &csbiInfo);
-
-	FillConsoleOutputCharacter(hConsoleOut, ' ', csbiInfo.dwSize.X * csbiInfo.dwSize.Y,
-
-Home, &dummy);
-	csbiInfo.dwCursorPosition.X = 0;
-	csbiInfo.dwCursorPosition.Y = 0;
-	SetConsoleCursorPosition(hConsoleOut, csbiInfo.dwCursorPosition);
-}
-
 // Ham ve background
 void DrawBackground() {
 	Color(13);
@@ -148,7 +130,6 @@ void WriteHighScore(int a)
 // Ham tao gia tri ban dau
 void Start_Map()
 {
-	DeleteScreen();
 	DrawBackground();
 
 	Score = 0;
